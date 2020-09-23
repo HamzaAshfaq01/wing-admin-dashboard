8158909b928ba942dba22978a0293f3c3e1e6127
Nexus <tayyabkhalid369@gmail.com>
2020-09-23 11:17:53 -0700

    Updates

---
+++ precache-manifest.cee07bdb269ac8cf80a3b624b62b5567.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17dde50f895796e1c6786a16ca3f9d7",
+    "url": "/wing-admin-dashboard/index.html"
+  },
+  {
+    "revision": "4467272259c0edaa6c97",
+    "url": "/wing-admin-dashboard/static/css/2.23423af4.chunk.css"
+  },
+  {
+    "revision": "0253fd7708163c189cc7",
+    "url": "/wing-admin-dashboard/static/css/main.d602ed16.chunk.css"
+  },
+  {
+    "revision": "4467272259c0edaa6c97",
+    "url": "/wing-admin-dashboard/static/js/2.371c7bbb.chunk.js"
+  },
+  {
+    "revision": "74576634b1ad091cd02d1c3a3bb53d12",
+    "url": "/wing-admin-dashboard/static/js/2.371c7bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0253fd7708163c189cc7",
+    "url": "/wing-admin-dashboard/static/js/main.a09f7645.chunk.js"
+  },
+  {
+    "revision": "7d273666c1ffa827e6b7",
+    "url": "/wing-admin-dashboard/static/js/runtime-main.2c2415d0.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/wing-admin-dashboard/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/wing-admin-dashboard/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "a6f24e7dd5470a8bd2f48ebb53dbe8d5",
+    "url": "/wing-admin-dashboard/static/media/damir-bosnjak.a6f24e7d.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/wing-admin-dashboard/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "06dff5bf9a47c9fd79bea1b4f835b521",
+    "url": "/wing-admin-dashboard/static/media/logo.06dff5bf.svg"
+  },
+  {
+    "revision": "aab414f7b69d0d4ad897e75e735f240a",
+    "url": "/wing-admin-dashboard/static/media/mike.aab414f7.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/wing-admin-dashboard/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/wing-admin-dashboard/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/wing-admin-dashboard/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/wing-admin-dashboard/static/media/nucleo-icons.f32ed5ad.eot"
+  }
+]);

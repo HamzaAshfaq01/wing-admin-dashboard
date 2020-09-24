703b4ad91208c2816603df9ff6bb1e1ce9d96c81
Nexus <tayyabkhalid369@gmail.com>
2020-09-24 15:11:40 -0700

    changed default router

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Route path="/" render={(props) => <AdminLayout {...props} />} />
     </Switch>
   </Router>,
   document.getElementById("root")
